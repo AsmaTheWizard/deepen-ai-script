d783fea4be6e6abd5d45e1a06a8f7c47415b9640
NithinDonepudi <51224949+NithinDonepudi@users.noreply.github.com>
2022-11-03 19:47:53 -0400

    Update GPSCoordinate.py

--- util/GPSCoordinate.py
+++ util/GPSCoordinate.py
@@ -1,2 +1,27 @@
+import json
+import os
+import math
+import sys
 
-#TODO: Nithan
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+directory=input_path
+list=[]
+for filename in os.listdir(directory):
+    f = open(os.path.join(directory, filename))
+    data = json.load(f)
+    dictionary={
+        "title":directory,
+        "timestamp":data["timestamp"],
+        "latitude":data["gnss"]["latitude"]*180/math.pi,
+        "longitude":data["gnss"]["longitude"]*180/math.pi,
+        "altitude":data["gnss"]["altitude"]
+    }
+    list.append(dictionary)
+
+with open(output_path+"\output.json", "w") as outfile:
+    json.dump(list, outfile)
+f.close()
+
+
